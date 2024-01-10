41bddb74b8a897ba48b0d3454cf8348c74b4b39b
Kerven Kildhery <kervensilvva@gmail.com>
2024-01-10 14:34:35 -0300

    inserção de usuarios com post

--- src/main/java/com/kerven/workshop/services/UserService.java
+++ src/main/java/com/kerven/workshop/services/UserService.java
@@ -1,10 +1,12 @@
 package com.kerven.workshop.services;
 
 import com.kerven.workshop.domain.User;
+import com.kerven.workshop.dtos.UserDTO;
 import com.kerven.workshop.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.kerven.workshop.repositories.UserRepository;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,4 +30,13 @@ public class UserService{
         }
 
     }
+
+    public User insert(User obj) {
+        return userRepository.insert(obj);
+    }
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
+
+
 }
