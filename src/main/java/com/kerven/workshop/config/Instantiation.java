ecf0c3840ef0838f1f09bc91c53d53ef76822d2e
Kerven Kildhery <kervensilvva@gmail.com>
2024-01-11 14:12:55 -0300

    projeção dos dados do autor com DTO

--- src/main/java/com/kerven/workshop/config/Instantiation.java
+++ src/main/java/com/kerven/workshop/config/Instantiation.java
@@ -2,6 +2,8 @@ package com.kerven.workshop.config;
 
 import com.kerven.workshop.domain.Post;
 import com.kerven.workshop.domain.User;
+import com.kerven.workshop.dtos.AuthorDTO;
+import com.kerven.workshop.dtos.UserDTO;
 import com.kerven.workshop.repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
@@ -36,10 +38,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
